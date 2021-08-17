f2395ab12ff3669048b48609117713ac549f21f0
tunailgaz <tunailgaz@gmail.com>
2021-08-18 02:55:11 +0300

    wip

--- install.sh
+++ install.sh
@@ -1,31 +1,11 @@
 #!/usr/bin/env bash
 
-create_symlinks() {
-    dotfile_src=$1
-    dotfile_dst=$2
-    if [[ "$dotfile_dst" != /*  ]]; then
-        # relative path
-        dotfile_dst=$HOME/$dotfile_dst
-    fi
-    if [ -e $dotfile_dst ]; then
-        if [ -h $dotfile_dst ]; then
-            ln -sf $PWD/$dotfile_src $dotfile_dst
-            echo "Update existed symlink $dotfile_dst"
-        else
-            echo "[WARN] Ignore due to $dotfile_dst exists and is not a symlink"
-        fi
-    else
-        ln -sf $PWD/$dotfile_src $dotfile_dst
-        echo "Create symlink $dotfile_dst"
-    fi
-}
-
 config_git() {
-    create_symlinks "git/gitconfig" "~/.gitconfig"
+    ln -sf  "git/gitconfig" "~/.gitconfig"
 }
 
 config_aliases() {
-    create_symlinks "aliases/memphis" "~/.memphis_profile"
+     ln -sf  "aliases/memphis" "~/.memphis_profile"
 }
 
 config_aliases

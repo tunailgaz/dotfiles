0053ba78a36728525ba88bf88a6a96f79c3cb94e
tunailgaz <tunailgaz@gmail.com>
2021-08-18 02:57:58 +0300

    wip

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 config_git() {
-    cp  "$(pwd)git/gitconfig" "~/.gitconfig"
+    cp  "$(pwd)/git/gitconfig" "~/.gitconfig"
 }
 
 config_aliases() {
-     cp  "$(pwd)aliases/memphis" "~/.memphis_profile"
+     cp  "$(pwd)/aliases/memphis" "~/.memphis_profile"
 }
 
 config_aliases

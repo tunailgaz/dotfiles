f6ea13a050c7717ff29aa2c4b3d7d1316e57db3e
tunailgaz <tunailgaz@gmail.com>
2021-08-18 03:21:28 +0300

    wip

--- install.sh
+++ install.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 
-#create_symlinks() {
-#    dotfile_src=$1
-#    dotfile_dst=$2
-#    if [[ "$dotfile_dst" != /*  ]]; then
-#        # relative path
-#        dotfile_dst=$HOME/$dotfile_dst
-#    fi
-#    if [ -e $dotfile_dst ]; then
-#        if [ -h $dotfile_dst ]; then
-#            ln -sf $PWD/$dotfile_src $dotfile_dst
-#            echo "Update existed symlink $dotfile_dst"
-#        else
-#            echo "[WARN] Ignore due to $dotfile_dst exists and is not a symlink"
-#        fi
-#    else
-#        ln -sf $PWD/$dotfile_src $dotfile_dst
-#        echo "Create symlink $dotfile_dst"
-#    fi
-#}
+create_symlinks() {
+    dotfile_src=$1
+    dotfile_dst=$2
+    if [[ "$dotfile_dst" != /*  ]]; then
+        # relative path
+        dotfile_dst=$HOME/$dotfile_dst
+    fi
+    if [ -e $dotfile_dst ]; then
+        if [ -h $dotfile_dst ]; then
+            ln -sf $PWD/$dotfile_src $dotfile_dst
+            echo "Update existed symlink $dotfile_dst"
+        else
+            echo "[WARN] Ignore due to $dotfile_dst exists and is not a symlink"
+        fi
+    else
+        ln -sf $PWD/$dotfile_src $dotfile_dst
+        echo "Create symlink $dotfile_dst"
+    fi
+}
 
 config_aliases() {
-   ln -s -f  "$(pwd)/aliases/memphis" "~/.memphis_profile"
+   create_symlinks "aliases/memphis" "~/.memphis_profile"
 }
 config_git() {
-  ln -s -f "$(pwd)/git/gitconfig" "~/.gitconfig"
+   create_symlinks "git/gitconfig" "~/.gitconfig"
 }
 
 config_aliases
